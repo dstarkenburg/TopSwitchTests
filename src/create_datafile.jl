f0b4de42503b0cab277fe1b6ab422525e16b6189
Duncan Starkenburg <duncan@starkenburg.com>
2025-08-18 14:31:45 -0700

    Updates

--- src/create_datafile.jl
+++ src/create_datafile.jl
@@ -42,109 +42,111 @@ function generate_pd_qd!(data, data_copy)
     return data
 end
 
-h5open(h5write_filename, "w") do file
-    write_dataset(file, "alpha_max", alpha_max)
-    write_dataset(file, "alpha_min", alpha_min)
-    write_dataset(file, "total_samples", total_samples)
-    write_dataset(file, "perturb_percent", perturb_percent)
+file = h5open(h5write_filename, "w")
+write_dataset(file, "alpha_max", alpha_max)
+write_dataset(file, "alpha_min", alpha_min)
+write_dataset(file, "total_samples", total_samples)
+write_dataset(file, "perturb_percent", perturb_percent)
 
-    g_test = create_group(file, "test_data")
-    write_dataset(g_test, "index", [1])
-    write_dataset(g_test, "num_samples", [n_test])
-    for i in 1:n_test
-        alpha = alpha_min + (alpha_max - alpha_min) * rand()
-        generate_risk!(data, alpha)
-        generate_pd_qd!(data, data_copy)
+g_test = create_group(file, "test_data")
+write_dataset(g_test, "index", [1])
+write_dataset(g_test, "num_samples", [n_test])
+for i in 1:n_test
+    alpha = alpha_min + (alpha_max - alpha_min) * rand()
+    generate_risk!(data, alpha)
+    generate_pd_qd!(data, data_copy)
 
-        group = create_group(g_test, string(i))
+    group = create_group(g_test, string(i))
 
-        # Load data
-        load = create_group(group, "load")
-        size = length(keys(data["load"]))
-        (qd_vals, pd_vals) = (Array{Float32}(undef, size), Array{Float32}(undef, size))
-        for (key, value) in data["load"]
-            qd_vals[parse(Int, key)] = value["qd"]
-            pd_vals[parse(Int, key)] = value["pd"]
-        end
-        write_dataset(load, "qd", qd_vals)
-        write_dataset(load, "pd", pd_vals)
-
-        # Branch data
-        branch = create_group(group, "branch")
-        prisk = Array{Float32}(undef, size)
-        for (key, value) in data["branch"]
-            prisk[parse(Int, key)] = data["branch"][key]["power_risk"]
-        end
-        write_dataset(branch, "power_risk", prisk)
-        
-        # Alpha
-        write_dataset(group, "alpha", [data["risk_weight"]])
+    # Load data
+    load = create_group(group, "load")
+    load_size = length(keys(data["load"]))
+    (qd_vals, pd_vals) = (Array{Float32}(undef, load_size), Array{Float32}(undef, load_size))
+    for (key, value) in data["load"]
+        qd_vals[parse(Int, key)] = value["qd"]
+        pd_vals[parse(Int, key)] = value["pd"]
     end
+    write_dataset(load, "qd", qd_vals)
+    write_dataset(load, "pd", pd_vals)
 
-    g_train = create_group(file, "train_data")
-    write_dataset(g_train, "index", [1])
-    write_dataset(g_train, "num_samples", [n_train])
-    for i in 1:n_train
-        alpha = alpha_min + (alpha_max - alpha_min) * rand()
-        generate_risk!(data, alpha)
-        generate_pd_qd!(data, data_copy)
-
-        group = create_group(g_train, string(i))
-
-        # Load data
-        load = create_group(group, "load")
-        size = length(keys(data["load"]))
-        (qd_vals, pd_vals) = (Array{Float32}(undef, size), Array{Float32}(undef, size))
-        for (key, value) in data["load"]
-            qd_vals[parse(Int, key)] = value["qd"]
-            pd_vals[parse(Int, key)] = value["pd"]
-        end
-        write_dataset(load, "qd", qd_vals)
-        write_dataset(load, "pd", pd_vals)
-
-        # Branch data
-        branch = create_group(group, "branch")
-        prisk = Array{Float32}(undef, size)
-        for (key, value) in data["branch"]
-            prisk[parse(Int, key)] = data["branch"][key]["power_risk"]
-        end
-        write_dataset(branch, "power_risk", prisk)
-        
-        # Alpha
-        write_dataset(group, "alpha", [data["risk_weight"]])
+    # Branch data
+    branch = create_group(group, "branch")
+    branch_size = length(keys(data["branch"]))
+    prisk = Array{Float32}(undef, branch_size)
+    for (key, value) in data["branch"]
+        prisk[parse(Int, key)] = data["branch"][key]["power_risk"]
     end
-
-    g_val = create_group(file, "val_data")
-    write_dataset(g_val, "index", [1])
-    write_dataset(g_val, "num_samples", [n_val])
-    for i in 1:n_val
-        alpha = alpha_min + (alpha_max - alpha_min) * rand()
-        generate_risk!(data, alpha)
-        generate_pd_qd!(data, data_copy)
-
-        group = create_group(g_val, string(i))
-
-        # Load data
-        load = create_group(group, "load")
-        size = length(keys(data["load"]))
-        (qd_vals, pd_vals) = (Array{Float32}(undef, size), Array{Float32}(undef, size))
-        for (key, value) in data["load"]
-            qd_vals[parse(Int, key)] = value["qd"]
-            pd_vals[parse(Int, key)] = value["pd"]
-        end
-        write_dataset(load, "qd", qd_vals)
-        write_dataset(load, "pd", pd_vals)
-
-        # Branch data
-        branch = create_group(group, "branch")
-        prisk = Array{Float32}(undef, 0, size)
-        for (key, value) in data["branch"]
-            prisk[parse(Int, key)] = data["branch"][key]["power_risk"]
-        end
-        write_dataset(branch, "power_risk", prisk)
+    write_dataset(branch, "power_risk", prisk)
         
-        # Alpha
-        write_dataset(group, "alpha", [data["risk_weight"]])
-    end
-    close(file)
+    # Alpha
+    write_dataset(group, "alpha", [data["risk_weight"]])
 end
+
+g_train = create_group(file, "train_data")
+write_dataset(g_train, "index", [1])
+write_dataset(g_train, "num_samples", [n_train])
+for i in 1:n_train
+    alpha = alpha_min + (alpha_max - alpha_min) * rand()
+    generate_risk!(data, alpha)
+    generate_pd_qd!(data, data_copy)
+
+    group = create_group(g_train, string(i))
+
+    # Load data
+    load = create_group(group, "load")
+    load_size = length(keys(data["load"]))
+    (qd_vals, pd_vals) = (Array{Float32}(undef, load_size), Array{Float32}(undef, load_size))
+    for (key, value) in data["load"]
+        qd_vals[parse(Int, key)] = value["qd"]
+        pd_vals[parse(Int, key)] = value["pd"]
+    end
+    write_dataset(load, "qd", qd_vals)
+    write_dataset(load, "pd", pd_vals)
+
+    # Branch data
+    branch = create_group(group, "branch")
+    branch_size = length(keys(data["branch"]))
+    prisk = Array{Float32}(undef, branch_size)
+    for (key, value) in data["branch"]
+        prisk[parse(Int, key)] = data["branch"][key]["power_risk"]
+    end
+    write_dataset(branch, "power_risk", prisk)
+    
+    # Alpha
+    write_dataset(group, "alpha", [data["risk_weight"]])
+end
+
+g_val = create_group(file, "val_data")
+write_dataset(g_val, "index", [1])
+write_dataset(g_val, "num_samples", [n_val])
+for i in 1:n_val
+    alpha = alpha_min + (alpha_max - alpha_min) * rand()
+    generate_risk!(data, alpha)
+    generate_pd_qd!(data, data_copy)
+
+    group = create_group(g_val, string(i))
+
+    # Load data
+    load = create_group(group, "load")
+    load_size = length(keys(data["load"]))
+    (qd_vals, pd_vals) = (Array{Float32}(undef, load_size), Array{Float32}(undef, load_size))
+    for (key, value) in data["load"]
+        qd_vals[parse(Int, key)] = value["qd"]
+        pd_vals[parse(Int, key)] = value["pd"]
+    end
+    write_dataset(load, "qd", qd_vals)
+    write_dataset(load, "pd", pd_vals)
+
+    # Branch data
+    branch = create_group(group, "branch")
+    branch_size = length(keys(data["branch"]))
+    prisk = Array{Float32}(undef, branch_size)
+    for (key, value) in data["branch"]
+        prisk[parse(Int, key)] = data["branch"][key]["power_risk"]
+    end
+    write_dataset(branch, "power_risk", prisk)
+    
+    # Alpha
+    write_dataset(group, "alpha", [data["risk_weight"]])
+end
+close(file)
